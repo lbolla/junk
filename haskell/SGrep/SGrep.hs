7476280273da7f578e3df82f407681efceafd00f
Lorenzo Bolla <lbolla@gmail.com>
2011-11-27 15:51:03 +0000

    use MaybeT

--- haskell/SGrep/SGrep.hs
+++ haskell/SGrep/SGrep.hs
@@ -14,6 +14,8 @@ module Main where
 import Prelude hiding (catch)
 import Control.Exception (finally)
 import Control.Monad (unless)
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
 import Data.List (isPrefixOf)
 import Data.Maybe (isNothing, fromJust)
 import System.Directory (getTemporaryDirectory, removeFile)
@@ -59,8 +61,8 @@ goToBOL h = do
 getCurrentLine :: Handle -> IO String
 getCurrentLine h = goToBOL h >> hGetLine h
 
-getPrevLine :: Handle -> IO (Maybe String)
-getPrevLine h = do
+getPrevLine :: Handle -> MaybeT IO String
+getPrevLine h = MaybeT $ do
         goToBOLAndDo h $ do
                 hSeek h RelativeSeek (-2)
                 goToBOLAndDo h $ do
@@ -78,26 +80,27 @@ getPrevLine h = do
 goTo :: Handle -> Integer -> IO ()
 goTo h = hSeek h AbsoluteSeek
 
-search :: Chunk -> String -> IO (Maybe String)
+search :: Chunk -> String -> MaybeT IO String
 search (Chunk h start end) str
-        | start >= end = return Nothing
-        | otherwise = if mid == (end - 1)
-                         then return Nothing
-                         else do
-                                 goTo h mid
-                                 midLine <- getCurrentLine h
-                                 prevLine <- getPrevLine h
-                                 if str `isPrefixOf` midLine && (isNothing prevLine || not (str `isPrefixOf` fromJust prevLine))
-                                    then return $ Just midLine
-                                    else if str < midLine
-                                            then search (Chunk h start mid) str
-                                            else search (Chunk h mid end) str
+        | start >= end = MaybeT $ return Nothing
+        | otherwise = MaybeT $ do
+                if mid == (end - 1)
+                   then return Nothing
+                   else do
+                           goTo h mid
+                           midLine <- liftIO $ getCurrentLine h
+                           prevLine <- runMaybeT $ getPrevLine h
+                           if str `isPrefixOf` midLine && (isNothing prevLine || not (str `isPrefixOf` fromJust prevLine))
+                              then return $ Just midLine
+                              else if str < midLine
+                                      then runMaybeT $ search (Chunk h start mid) str
+                                      else runMaybeT $ search (Chunk h mid end) str
            where mid = (start + end) `div` 2
 
 sgrep :: String -> Handle -> IO ()
 sgrep s h = do
         len <- hFileSize h
-        _ <- search (Chunk h 0 len) s
+        _ <- runMaybeT $ search (Chunk h 0 len) s
         --  putStrLn $ show match
         c <- hGetContents h
         putStrLn . unlines $ takeWhile (isPrefixOf s) (lines c)
