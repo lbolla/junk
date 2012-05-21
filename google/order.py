a3c6f7ed8e34c979fa26a27023c9ad4b22221eb2
Lorenzo Bolla <lbolla@gmail.com>
2012-05-21 21:34:41 +0100

    order fixed

--- google/order.py
+++ google/order.py
@@ -4,14 +4,14 @@ from qsort import partition as partition
 
 def order(A, p, r, i):
     '''Return the i-th smallest element of A[p:r+1].'''
+
+    if p == r:
+        return A[p]
+
     q = partition(A, p, r)
     k = q - p + 1
 
-    # TODO
-    if i == 0:
-        return A[p]
-
-    if i < k:
+    if i <= k:
         return order(A, p, q, i)
     else:
         return order(A, q + 1, r, i - k)
@@ -19,6 +19,8 @@ def order(A, p, r, i):
 
 A = [4,6,5,2,1,3,4,5]
 print sorted(A)
-# print order(A, 0, len(A) - 1, 2)  # 2
-# print order(A, 0, len(A) - 1, 1)  # 1
+print order(A, 0, len(A) - 1, 1)  # 1
+print order(A, 0, len(A) - 1, 2)  # 2
 print order(A, 0, len(A) - 1, 3)  # 3
+print order(A, 0, len(A) - 1, 5)  # 4
+print order(A, 0, len(A) - 1, 6)  # 5
