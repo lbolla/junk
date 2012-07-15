e5df06eb982cbcc93052738a7aa61683713f54ee
Lorenzo Bolla <lbolla@gmail.com>
2012-07-15 19:03:32 +0100

    tests for finite maps

--- haskell/Okasaki/src/OkasakiTester.hs
+++ haskell/Okasaki/src/OkasakiTester.hs
@@ -5,6 +5,7 @@ import Test.Framework (defaultMain, testGroup)
 import qualified Heap
 import qualified Stack
 import qualified Set
+import qualified FiniteMap
 
 main :: IO ()
 main = defaultMain tests
@@ -13,4 +14,5 @@ tests = [
         testGroup "Stack" Stack.tests
       , testGroup "Set" Set.tests
       , testGroup "Heap" Heap.tests
+      , testGroup "FiniteMap" FiniteMap.tests
     ]
