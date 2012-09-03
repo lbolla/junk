6f4efd262880125433878f27a6fee319420e10dc
Lorenzo Bolla <lbolla@gmail.com>
2012-09-03 16:22:27 +0100

    moved Okasaki's stuff in its own repo

--- haskell/Okasaki/src/Okasaki/FiniteMap.hs
+++
@@ -1,6 +0,0 @@
-module Okasaki.FiniteMap(FiniteMap(..)) where
-
-class FiniteMap m where
-    empty :: m k v
-    bind :: Ord k => k -> v -> m k v -> m k v
-    lookup :: Ord k => k -> m k v -> Maybe v
--- haskell/Okasaki/src/Okasaki/Stack.hs
+++
@@ -1,8 +0,0 @@
-module Okasaki.Stack (Stack(..)) where
-
-class Stack s where
-    empty :: s a
-    isEmpty :: s a -> Bool
-    cons :: a -> s a -> s a
-    hd :: s a -> a
-    tl :: s a -> s a
